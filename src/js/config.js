7b48307049ada7f00aa4139acf04d1409100761c
SanaanAshfaq <sanaanashfaq@gmail.com>
2021-10-28 13:42:51 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'; 
+export const TIMEOUT_SEC = 50;
+export const RES_PER_PAGE = 10;
+export const KEY = '27161069-c3a2-49c3-85e4-ddfae0bc8341';
+export const MODAL_CLOSE_SEC = 2.5;
